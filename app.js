eabf5f40ae2f6cd380a0f387fca66fe201975736
Ahmed Hussain <144009774+its-ahmyy@users.noreply.github.com>
2026-04-19 04:23:49 +0500

    changes made

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("LEVEL-2 Deployed🔥");
+  res.end("LEVEL-2 FULLY WORKING ");
 });
 
 server.listen(3000, () => {
